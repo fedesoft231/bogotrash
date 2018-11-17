f7bcc0fa6e94640a5d2be2fb305555d17c3b1967
fhfonsecaa <fhfonsecaa@gmail.com>
2018-11-17 14:09:49 -0500

    Create rest provider

---
+++ frontend/bogotrash_frontend/src/providers/rest/rest.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  host = "http://localhost:8000/";
+  loginService = "api/login/";
+
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+  login(data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(this.host + this.loginService, data)
+        .subscribe(res => {
+          resolve(res);
+        }, (err) => {
+          reject(err);
+        });
+    });
+
+  }
+
+}
