b54af8462ad8d56ab62178b7c581431de07dd728
fhfonsecaa <fhfonsecaa@gmail.com>
2018-11-17 17:09:01 -0500

    Disposure center page creation empty

---
+++ frontend/bogotrash_frontend/src/pages/disposurecenter/disposurecenter.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { DisposurecenterPage } from './disposurecenter';
+
+@NgModule({
+  declarations: [
+    DisposurecenterPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(DisposurecenterPage),
+  ],
+})
+export class DisposurecenterPageModule {}
